8a9dd2c417599f40d5c0c3ca5ad143a344aed40c
Kyaw Kyaw Soe <kyawkyawsoe2212@gmail.com>
2026-06-20 21:30:45 +0900

    git test

--- src/main/java/com/example/exercise/controller/UserController.java
+++ src/main/java/com/example/exercise/controller/UserController.java
@@ -12,6 +12,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 
 @RestController
@@ -48,6 +50,11 @@ public class UserController {
     @PostMapping("/login")
     public String userLogin(@RequestBody Users user) {
         return userService.verifyUser(user);
+    }
+    
+    @GetMapping("/gitTest")
+    public String getMethodName() {
+        return "Welcome to GitHub!";
     }
     
 }
